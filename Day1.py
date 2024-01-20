bcbad2f4d7afec8fc24dfd740e93aef455a6e17d
Priyanshu <abpriyanshu007@gmail.com>
2024-01-20 23:20:10 +0530

    #EE_Day 0️⃣1️⃣

--- Day1.py
+++ Day1.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 import keras
 from keras import layers
 
+# Data Loading
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 
+# Data Flattening
 X_train_flattened= X_train.reshape(len(X_train), 28*28)
 X_test_flattened= X_test.reshape(len(X_test), 28*28)
 
@@ -11,6 +13,7 @@ model = keras.Sequential([
     keras.layers.Dense(10, input_shape=(784,), activation='sigmoid')
 ])
 
+# Model Compilation
 model.compile(
     optimizer= 'adam',
     loss= 'sparse_categorical_crossentropy',
